eb6dcbb3c475976f761593d26e61aa8d2c684ce8
masayuki-miyauchi <miyauchi0623@gmail.com>
2016-08-14 11:08:40 +0900

    設定修正

--- src/main/java/com/miya/masa/WebConfig.java
+++ src/main/java/com/miya/masa/WebConfig.java
@@ -6,14 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import lombok.extern.slf4j.Slf4j;
 
-@EnableWebMvc
 @Configuration
 @Slf4j
 public class WebConfig extends WebMvcConfigurerAdapter {
