7112683f4b3dc338a4b48b7bd2a7526e52575f7e
masayuki miyauchi <masayuki-miyauchi@mamezou>
2016-08-16 18:20:24 +0900

    一部redux反映

--- src/main/javascript/main.js
+++ src/main/javascript/main.js
@@ -19,11 +19,25 @@
 /* eslint-env browser,jquery global */
 /* eslint no-unused-vars: ["error", {"varsIgnorePattern": "React"}] */
 
-import Todo from './todo/container/TodoContainer.jsx';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import Todo from './container/TodoContainer.jsx';
+import configureStore from './store/configureStore'
 import injectTapEventPlugin from 'react-tap-event-plugin';
+
 injectTapEventPlugin();
+
+const store = configureStore()
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('contents')
+)
+
 
 ReactDOM.render(
   <Todo />,
